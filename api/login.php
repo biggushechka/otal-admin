e6bd799f4562fed1f1204b23f30bd1b8c7d319d2
gorbatenkomax.formula@yandex.ru <gorbatenkomax.formula@yandex.ru>
2023-12-15 10:56:23 +0300

    update

--- api/login.php
+++ api/login.php
@@ -6,7 +6,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 require_once $rootPath . '/api/db_connect.php';
 
 $login = $_POST['login'];
-$password = $_POST['password'];
+$password = hash('sha256', $_POST['password']);
 
 if ($method === "POST") {
     $sth = $dbh->prepare("SELECT * FROM `users` WHERE `login` = :login LIMIT 1");
@@ -25,11 +25,9 @@ if ($method === "POST") {
             echo json_encode(['user'=>'не правильный пароль'], JSON_UNESCAPED_UNICODE);
         }
     }
-
-    die();
 } else {
     header("HTTP/1.1 404 Not Found");
     echo json_encode(['user'=>null], JSON_UNESCAPED_UNICODE);
 }
-
+$dbh = null;
 die();
