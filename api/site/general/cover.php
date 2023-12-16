f2ca65c859de26a7b6cffdcc3e7ff357fb758dc5
MaxXam715 <gorbatenkomax@yandex.ru>
2023-12-17 00:54:42 +0300

    update

--- api/site/general/cover.php
+++ api/site/general/cover.php
@@ -43,7 +43,7 @@ if ($method === "POST") {
         ]);
         $album = $query_get_album->fetch(PDO::FETCH_OBJ);
 
-        echo $album->id_album;
+        print_r($album->id_album);
 
         // добавляем фото в альбом
         $query_add_cover = $dbh->prepare("INSERT INTO `project_photos` SET `id_album` = :id_album, `id_site` = :id_site, `title` = :title, `extension` = :extension, `image` = :image, `activity` = :activity, `date_create` = :date_create");
