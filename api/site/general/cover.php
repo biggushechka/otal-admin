1ba892ef4a0673974a4166da3799b1efc7ab25ce
MaxXam715 <gorbatenkomax@yandex.ru>
2023-12-17 02:02:48 +0300

    update

--- api/site/general/cover.php
+++ api/site/general/cover.php
@@ -39,11 +39,11 @@ if ($method === "POST") {
         $insert_id = $dbh->lastInsertId();
 
         // получаем альбом
-        $query_get_album = $dbh->prepare("SELECT `id_album` FROM `project_albums` WHERE id = :id");
+        $query_get_album = $dbh->prepare("SELECT `id_album` FROM `project_albums` WHERE id = :id LIMIT 1");
         $query_get_album->execute(["id" => $insert_id]);
-        $id_album = $query_get_album->fetch();
+        $album = $query_get_album->fetch(PDO::FETCH_OBJ);
 
-        echo $id_album['id_album'];
+        echo $album['id_album'];
 
         if ($query_get_album) {
             echo "запрос выполнился";
