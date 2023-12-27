caa1983b7c18948be5f4784deee772c834509f2f
gorbatenkomax.formula@yandex.ru <gorbatenkomax.formula@yandex.ru>
2023-12-27 16:32:39 +0300

    update

--- api/site/content/general.php
+++ api/site/content/general.php
@@ -9,7 +9,7 @@ require_once $rootPath . '/backend/functions.php';
 $get_post_data = file_get_contents("php://input");
 $POST = json_decode($get_post_data, true);
 
-$id = $POST['id'] ?? $_GET['id'];
+$domain = $POST['id'] ?? $_GET['id'];
 
 if ($method === "POST") {
     header("HTTP/1.1 200 OK");
