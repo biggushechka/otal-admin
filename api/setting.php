33499ab2198a9f20d41b02d9fa4cab295c57676b
gorbatenkomax.formula@yandex.ru <gorbatenkomax.formula@yandex.ru>
2023-12-27 16:42:27 +0300

    update

--- api/setting.php
+++ api/setting.php
@@ -11,6 +11,7 @@ $allowedOrigins = [
 // Устанавливаем возможность отправлять ответ для разрешенных доменов
 if (in_array($origin, $allowedOrigins)) {
     header("Access-Control-Allow-Origin: " . $origin);
+    header('Access-Control-Allow-Methods: POST');
     header("Access-Control-Allow-Credentials: true");
 } else {
     // Сайт, не входящий в список разрешенных, получит ошибку доступа
