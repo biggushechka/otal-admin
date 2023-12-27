a643c012cbddcb2216d86c4223e4dfded121e428
gorbatenkomax.formula@yandex.ru <gorbatenkomax.formula@yandex.ru>
2023-12-27 16:47:32 +0300

    update

--- api/setting.php
+++ api/setting.php
@@ -4,17 +4,19 @@ $origin = $_SERVER['HTTP_ORIGIN'];
 
 // разрешенные домены
 $allowedOrigins = [
-    '',
     'http://odal'
 ];
 
 // Устанавливаем возможность отправлять ответ для разрешенных доменов
-if (in_array($origin, $allowedOrigins)) {
-    header("Access-Control-Allow-Origin: " . $origin);
-    header('Access-Control-Allow-Methods: POST');
-    header("Access-Control-Allow-Credentials: true");
-} else {
-    // Сайт, не входящий в список разрешенных, получит ошибку доступа
-    header("HTTP/1.1 403 Forbidden");
-    exit("Доступ запрещен ((");
+
+if ($origin != "") {
+    if (in_array($origin, $allowedOrigins)) {
+        header("Access-Control-Allow-Origin: " . $origin);
+        header('Access-Control-Allow-Methods: POST');
+        header("Access-Control-Allow-Credentials: true");
+    } else {
+        // Сайт, не входящий в список разрешенных, получит ошибку доступа
+        header("HTTP/1.1 403 Forbidden");
+        exit("Доступ запрещен ((");
+    }
 }
