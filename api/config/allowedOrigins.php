10243e44dd785039be9c597ab3161637bde215c8
gorbatenkomax.formula@yandex.ru <gorbatenkomax.formula@yandex.ru>
2024-02-26 12:57:14 +0300

    update

--- api/config/allowedOrigins.php
+++ api/config/allowedOrigins.php
@@ -1,33 +1,28 @@
 <?php
 
+$rootPath = $_SERVER['DOCUMENT_ROOT'];
+
 header("Access-Control-Allow-Origin: https://alba-del-mare.ru");
 header("Access-Control-Allow-Credentials: true");
 
-$rootPath = $_SERVER['DOCUMENT_ROOT'];
-$referer = parse_url($_SERVER['HTTP_REFERER']);
-$refererDomain = $referer['host'];
+if(isset($_SERVER['HTTP_REFERER'])) {
+    $referer = parse_url($_SERVER['HTTP_REFERER']);
+    $refererDomain = $referer['host'];
+    $ip = gethostbyname($refererDomain);
+    $ip_convert = ip2long($ip);
+    $ip_decod = long2ip($ip_convert);
 
+    echo "refererDomain: $refererDomain <br>";
+    echo "IP-адрес сайта: $ip <br>";
+    echo "IP-адрес сайта (convert): $ip_convert <br>";
+    echo "IP-адрес сайта (decod): $ip_decod";
 
-$ip = gethostbyname($refererDomain);
-$ip_convert = ip2long($ip);
-$ip_decod = long2ip($ip_convert);
-
-echo "refererDomain: $refererDomain <br>";
-echo "IP-адрес сайта: $ip <br>";
-echo "IP-адрес сайта (convert): $ip_convert <br>";
-echo "IP-адрес сайта (decod): $ip_decod";
-
-
-//if(isset($_SERVER['HTTP_REFERER'])) {
-//    $referer = parse_url($_SERVER['HTTP_REFERER']);
-//    $refererDomain = $referer['host'];
-//
-//    // Выполните необходимые действия на основе значения $refererDomain
-//    echo "Запрос был отправлен с домена: " . $refererDomain;
-//} else {
-//    // Обработка случая, когда запрос не содержит HTTP_REFERER
-//    echo "HTTP_REFERER не определен";
-//}
+    // Выполните необходимые действия на основе значения $refererDomain
+    echo "Запрос был отправлен с домена: " . $refererDomain;
+} else {
+    // Обработка случая, когда запрос не содержит HTTP_REFERER
+    echo "HTTP_REFERER не определен";
+}
 
 
 
