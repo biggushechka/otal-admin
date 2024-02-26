ee527ba4b49e646299d804433a4ea06ec0d8baf0
gorbatenkomax.formula@yandex.ru <gorbatenkomax.formula@yandex.ru>
2024-02-26 13:25:47 +0300

    update

--- api/config/allowedOrigins.php
+++ api/config/allowedOrigins.php
@@ -1,8 +1,6 @@
 <?php
 
-global $dbh, $rootPath;
-
-require_once $rootPath . '/api/config/db_connect.php';
+global $dbh;
 
 $rootPath = $_SERVER['DOCUMENT_ROOT'];
 
@@ -10,8 +8,7 @@ if(isset($_SERVER['HTTP_REFERER'])) {
     $referer = parse_url($_SERVER['HTTP_REFERER']); // конвертирует URL в строку
     $refererDomain = $referer['host']; // получаем домен
     $ip = gethostbyname($refererDomain); // получаем IP-адрес по домену
-//    $ip_convert = ip2long($ip); // конвертируем IP-адрес
-    $ip_convert = 123123; // конвертируем IP-адрес
+    $ip_convert = ip2long($ip); // конвертируем IP-адрес
 
     // получение сайта
     $getSites = $dbh->prepare("SELECT * FROM `my_sites` WHERE `domain` = :domain AND `ip_address` = :ip_address LIMIT 1");
@@ -25,7 +22,8 @@ if(isset($_SERVER['HTTP_REFERER'])) {
         header("Access-Control-Allow-Credentials: true");
     } else {
         $dbh = null;
-        echo $dbh;
+        header("HTTP/1.1 403 Forbidden");
+        exit("Доступ запрещен ((");
     }
 
 } else {
--- api/site/content/index.php
+++ api/site/content/index.php
@@ -2,3 +2,4 @@
 
 $rootPath = $_SERVER['DOCUMENT_ROOT'];
 require_once $rootPath . '/api/config/allowedOrigins.php';
+require_once $rootPath . '/api/config/db_connect.php';
