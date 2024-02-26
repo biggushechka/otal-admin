742d6a569df2eae6fefefd0f70a464530f4abca4
gorbatenkomax.formula@yandex.ru <gorbatenkomax.formula@yandex.ru>
2024-02-26 12:50:46 +0300

    update

--- api/config/allowedOrigins.php
+++ api/config/allowedOrigins.php
@@ -7,9 +7,30 @@ $rootPath = $_SERVER['DOCUMENT_ROOT'];
 $referer = parse_url($_SERVER['HTTP_REFERER']);
 $refererDomain = $referer['host'];
 
-echo "refererDomain: $refererDomain";
 
-file_put_contents($rootPath . "/api/config/allowedOrigins.txt", $refererDomain);
+$ip = gethostbyname($refererDomain);
+$ip_orig = ip2long($ip);
+$ip_decod = long2ip($ip_orig);
+
+echo "refererDomain: $refererDomain";
+echo "IP-адрес сайта (orig): $ip_orig <br>";
+echo "IP-адрес сайта (decod): $ip_decod";
+
+
+//if(isset($_SERVER['HTTP_REFERER'])) {
+//    $referer = parse_url($_SERVER['HTTP_REFERER']);
+//    $refererDomain = $referer['host'];
+//
+//    // Выполните необходимые действия на основе значения $refererDomain
+//    echo "Запрос был отправлен с домена: " . $refererDomain;
+//} else {
+//    // Обработка случая, когда запрос не содержит HTTP_REFERER
+//    echo "HTTP_REFERER не определен";
+//}
+
+
+
+
 
 
 // разрешаем подключаться к API разрешенным доменам
--- api/site/content/index.php
+++ api/site/content/index.php
@@ -3,22 +3,3 @@
 $rootPath = $_SERVER['DOCUMENT_ROOT'];
 require_once $rootPath . '/api/config/allowedOrigins.php';
 require_once $rootPath . '/api/config/db_connect.php';
-
-//$ip = gethostbyname($domain);
-//$ip_orig = ip2long($ip);
-//$ip_decod = long2ip($ip_orig);
-
-//echo "IP-адрес сайта (orig): $ip_orig <br>";
-//echo "IP-адрес сайта (decod): $ip_decod";
-
-
-//if(isset($_SERVER['HTTP_REFERER'])) {
-//    $referer = parse_url($_SERVER['HTTP_REFERER']);
-//    $refererDomain = $referer['host'];
-//
-//    // Выполните необходимые действия на основе значения $refererDomain
-//    echo "Запрос был отправлен с домена: " . $refererDomain;
-//} else {
-//    // Обработка случая, когда запрос не содержит HTTP_REFERER
-//    echo "HTTP_REFERER не определен";
-//}
