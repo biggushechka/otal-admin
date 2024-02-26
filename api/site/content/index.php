86c7b64f02c6b814470a3dc2ae28b725a38ce01d
gorbatenkomax.formula@yandex.ru <gorbatenkomax.formula@yandex.ru>
2024-02-26 13:00:45 +0300

    update

--- api/site/content/index.php
+++ api/site/content/index.php
@@ -1,5 +1,5 @@
 <?php
 
 $rootPath = $_SERVER['DOCUMENT_ROOT'];
-require_once $rootPath . '/api/config/allowedOrigins.php';
 require_once $rootPath . '/api/config/db_connect.php';
+require_once $rootPath . '/api/config/allowedOrigins.php';
