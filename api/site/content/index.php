ed5a7d4bdc9cabaeff5259f35b7a833f87702031
gorbatenkomax.formula@yandex.ru <gorbatenkomax.formula@yandex.ru>
2024-02-26 13:44:29 +0300

    update

--- api/site/content/index.php
+++ api/site/content/index.php
@@ -1,23 +1,24 @@
 <?php
 
 global $dbh;
+
 $rootPath = $_SERVER['DOCUMENT_ROOT'];
 require_once $rootPath . '/api/config/db_connect.php';
 
 $rootPath = $_SERVER['DOCUMENT_ROOT'];
 
-if(isset($_SERVER['HTTP_REFERER'])) {
+if (isset($_SERVER['HTTP_REFERER'])) {
     $referer = parse_url($_SERVER['HTTP_REFERER']); // конвертирует URL в строку
     $refererDomain = $referer['host']; // получаем домен
     $ip = gethostbyname($refererDomain); // получаем IP-адрес по домену
     $ip_convert = ip2long($ip); // конвертируем IP-адрес
 
     // получение сайта
-    $getSites = $dbh->prepare("SELECT * FROM `my_sites` WHERE `domain` = :domain AND `ip_address` = :ip_address LIMIT 1");
-    $getSites->execute(["domain" => "https://" . $refererDomain, "ip_address" => $ip_convert]);
+    $getSite = $dbh->prepare("SELECT * FROM `my_sites` WHERE `domain` = :domain AND `ip_address` = :ip_address LIMIT 1");
+    $getSite->execute(["domain" => "https://" . $refererDomain, "ip_address" => $ip_convert]);
 
-    if ($getSites->rowCount() > 0) {
-        $site = $getSites->fetchAll(PDO::FETCH_ASSOC);
+    if ($getSite->rowCount() > 0) {
+        $site = $getSite->fetchAll(PDO::FETCH_ASSOC);
 
         // разрешаем подключаться к API разрешенным доменам
         header("Access-Control-Allow-Origin: https://" . $refererDomain);
@@ -32,4 +33,8 @@ if(isset($_SERVER['HTTP_REFERER'])) {
     $dbh = null;
     header("HTTP/1.1 403 Forbidden");
     exit("Доступ запрещен 222");
+}
+
+if ($dbh !== null) {
+    echo "dbh";
 }
