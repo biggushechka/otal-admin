fc8d00ae14b7e63a1b2886e069bba55ed6bf6285
gorbatenkomax.formula@yandex.ru <gorbatenkomax.formula@yandex.ru>
2024-02-26 12:47:20 +0300

    update

--- api/config/allowedOrigins.php
+++ api/config/allowedOrigins.php
@@ -1,5 +1,8 @@
 <?php
 
+header("Access-Control-Allow-Origin: https://alba-del-mare.ru");
+header("Access-Control-Allow-Credentials: true");
+
 $rootPath = $_SERVER['DOCUMENT_ROOT'];
 $referer = parse_url($_SERVER['HTTP_REFERER']);
 $refererDomain = $referer['host'];
@@ -8,8 +11,6 @@ echo "refererDomain: $refererDomain";
 
 file_put_contents($rootPath . "/api/config/allowedOrigins.txt", $refererDomain);
 
-header("Access-Control-Allow-Origin: https://alba-del-mare.ru");
-header("Access-Control-Allow-Credentials: true");
 
 // разрешаем подключаться к API разрешенным доменам
 //if (in_array($refererDomain, $allowedOrigins)) {
