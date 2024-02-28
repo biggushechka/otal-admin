a73f8146cf73e87fee91407509bcf67e3e099c87
Max <gorbatenkomax@yandex.ru>
2024-02-28 23:47:32 +0300

    update 33

--- api/site/content/advantages.php
+++ api/site/content/advantages.php
@@ -2,42 +2,4 @@
 
 function getAdvantages($id_site) {
     $generalInfo = new stdClass();
-
-    // собираем дату
-    $generalInfo->id_site = $id_site;
-    // end - собираем дату
-
-    // получаем общую информацию по проекту и контакты
-    $query_get_general = $dbh->prepare("SELECT * FROM `site_general` WHERE `id_site` = :id_site");
-    $query_get_general->execute(["id_site" => $id_site]);
-    $data_general = $query_get_general->fetch(PDO::FETCH_OBJ);
-
-    if ($query_get_general->rowCount() > 0) {
-        unset($data_general->id);
-        unset($data_general->id_site);
-        unset($data_general->preview_photo);
-        unset($data_general->date_update);
-
-        $generalInfo->title = $data_general->title_project;
-        $generalInfo->contacts = $data_general;
-
-        // получаем список параметров по проекту
-        $query_get_parameters = $dbh->prepare("SELECT * FROM `site_parameters` WHERE `id_site` = :id_site");
-        $query_get_parameters->execute(["id_site" => $id_site]);
-        $data_parameters = $query_get_parameters->fetch(PDO::FETCH_OBJ);
-
-        if ($query_get_parameters->rowCount() > 0) {
-            unset($data_parameters->id);
-            unset($data_parameters->id_site);
-
-            $generalInfo->parameters = $data_parameters;
-        }
-
-        header("HTTP/1.1 200 OK");
-        header('Content-Type: application/json; charset=UTF-8');
-        echo json_encode($generalInfo, JSON_UNESCAPED_UNICODE);
-    } else {
-        header("HTTP/1.1 404 Not Found");
-        echo json_encode("Site not found", JSON_UNESCAPED_UNICODE);
-    }
 }
