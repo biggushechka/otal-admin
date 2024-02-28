c87b8482566a46827f7a80afee75082c6099b858
Max <gorbatenkomax@yandex.ru>
2024-02-28 23:39:21 +0300

    update 33

--- api/site/content/get.php
+++ api/site/content/get.php
@@ -61,12 +61,12 @@ if ($dbh !== null) {
             getGeneral($id_site);
             break;
         case "advantages":
-            require_once "mainSlider.php";
-            getMainSlider($id_site);
-            break;
-        case "gallery":
             require_once "advantages.php";
             getAdvantages($id_site);
+            break;
+        case "gallery":
+            require_once "mainSlider.php";
+            getMainSlider($id_site);
             break;
         default:
             $dbh = null;
