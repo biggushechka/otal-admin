8d79385c8ffcaf983d2d60b5b19731c3e9851d48
Max <gorbatenkomax@yandex.ru>
2024-02-29 19:16:17 +0300

    update

--- api/site/content/about-project.php
+++ api/site/content/about-project.php
@@ -4,26 +4,24 @@ function aboutProject($id_site) {
     global $dbh;
     $about = new stdClass();
 
-    $query_get_about = $dbh->prepare("SELECT * FROM `site_desc_about` WHERE `id_site` = :id_site");
+    $query_get_about = $dbh->prepare("SELECT `title`, `desc` FROM `site_desc_about` WHERE `id_site` = :id_site");
     $query_get_about->execute(["id_site" => $id_site]);
 
     if ($query_get_about->rowCount() > 0) {
         $aboutProject = $query_get_about->fetch(PDO::FETCH_OBJ);
 
         // собираем дату
-        $about->about->title = $aboutProject->title;
-        $about->about->desc = $aboutProject->desc;
+        $about->about = $aboutProject;
     }
 
-    $query_get_territory = $dbh->prepare("SELECT * FROM `site_desc_territory` WHERE `id_site` = :id_site");
+    $query_get_territory = $dbh->prepare("SELECT `title`, `desc` FROM `site_desc_territory` WHERE `id_site` = :id_site");
     $query_get_territory->execute(["id_site" => $id_site]);
 
     if ($query_get_territory->rowCount() > 0) {
         $descTerritory = $query_get_territory->fetch(PDO::FETCH_OBJ);
 
         // собираем дату
-        $about->territory->title = $descTerritory->title;
-        $about->territory->desc = $descTerritory->desc;
+        $about->territory = $descTerritory;
     }
 
 
