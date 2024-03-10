854403cd15e03d603da663d4b1af7275a4c314d2
Max <gorbatenkomax@yandex.ru>
2024-03-10 12:32:46 +0300

    update

--- api/site/content/get.php
+++ api/site/content/get.php
@@ -44,13 +44,13 @@ if (isset($refererDom) && isset($_GET["domain"]) && $refererDom == "http://odal-
         header("Access-Control-Allow-Credentials: true");
     } else {
         $dbh = null;
-        header("HTTP/1.1 403 Forbidden");
+        header("HTTP/1.1 403 Forbidden1");
         exit("Доступ запрещен ((");
     }
 
 } else {
     $dbh = null;
-    header("HTTP/1.1 403 Forbidden");
+    header("HTTP/1.1 403 Forbidden2");
     exit("Доступ запрещен");
 }
 
