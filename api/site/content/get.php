0948c7d824f7d1a45c9b2881a57fafeda207d5e8
Max <gorbatenkomax@yandex.ru>
2024-03-10 12:28:58 +0300

    update

--- api/site/content/get.php
+++ api/site/content/get.php
@@ -1,5 +1,8 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+
 $rootPath = $_SERVER['DOCUMENT_ROOT'];
 $refererDom = $_SERVER['HTTP_REFERER'];
 $id_site = 0;
