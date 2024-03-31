015eeba9a851a36a6b8c704da4139b5d422078c3
Max <gorbatenkomax@yandex.ru>
2024-03-31 15:43:23 +0300

    update

--- api/site/orders/get-form-website.php
+++ api/site/orders/get-form-website.php
@@ -11,3 +11,7 @@ require_once $rootPath . '/api/config/db_connect.php';
 
 $get_post_data = file_get_contents("php://input");
 $POST = json_decode($get_post_data, true);
+
+header("HTTP/1.1 200 OK");
+header('Content-Type: application/json; charset=UTF-8');
+echo json_encode($POST, JSON_UNESCAPED_UNICODE);
