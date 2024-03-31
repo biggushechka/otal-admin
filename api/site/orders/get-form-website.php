6290e528cf7c745bd9c17175677eacdc0f6f95a8
Max <gorbatenkomax@yandex.ru>
2024-03-31 15:26:09 +0300

    update

--- api/site/orders/get-form-website.php
+++ api/site/orders/get-form-website.php
@@ -1,1 +1,9 @@
 <?php
+
+$rootPath = $_SERVER['DOCUMENT_ROOT'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+require_once $rootPath . '/api/config/db_connect.php';
+
+$get_post_data = file_get_contents("php://input");
+$POST = json_decode($get_post_data, true);
