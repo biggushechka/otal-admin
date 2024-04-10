96964d0e2ddfe48039404d080c5e073b170e94a9
Max <gorbatenkomax@yandex.ru>
2024-04-10 23:20:12 +0300

    update

--- api/test.php
+++ api/test.php
@@ -1,1 +1,22 @@
 <?php
+$rootPath = $_SERVER['DOCUMENT_ROOT'];
+
+
+// Подключаем библиотеку PHPMailer
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require $rootPath . '/vendor/autoload.php';
+
+$mail = new PHPMailer();
+$mail->setFrom('otalestate@support.com', 'Система'); // от кого (email и имя)
+$mail->addAddress('gorbatenkomax@yandex.ru', 'Recipient Name'); // кому (email и имя)
+
+$mail->isHTML(true);
+$mail->Subject = 'Subject';
+$mail->Body = 'Message body';
+
+if ($mail->send()) {
+    echo '✅ Письмо отправлено ))))';
+} else {
+    echo '❌ Ошибка при отправке....';
+}
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -18,6 +18,7 @@ class ComposerStaticInita702940bbf24a822224611db5c22942b
         'P' => 
         array (
             'ParagonIE\\ConstantTime\\' => 23,
+            'PHPMailer\\PHPMailer\\' => 20,
         ),
     );
 
@@ -29,6 +30,10 @@ class ComposerStaticInita702940bbf24a822224611db5c22942b
         'ParagonIE\\ConstantTime\\' => 
         array (
             0 => __DIR__ . '/..' . '/paragonie/constant_time_encoding/src',
+        ),
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
         ),
     );
 
