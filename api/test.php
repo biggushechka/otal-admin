9bda0ec0bf7fdc083ea990e84d68cd272c39fd13
Max <gorbatenkomax@yandex.ru>
2024-04-10 23:22:23 +0300

    update

--- api/test.php
+++ api/test.php
@@ -8,12 +8,16 @@ use PHPMailer\PHPMailer\Exception;
 require $rootPath . '/vendor/autoload.php';
 
 $mail = new PHPMailer();
+
+$mail->CharSet = 'UTF-8'; // Установка кодировки UTF-8
+$mail->setLanguage('ru', 'path_to_phpmailer/PHPMailer/language/'); // Задание языка сообщения (русский)
+
 $mail->setFrom('otalestate@support.com', 'Система'); // от кого (email и имя)
 $mail->addAddress('gorbatenkomax@yandex.ru', 'Recipient Name'); // кому (email и имя)
 
 $mail->isHTML(true);
-$mail->Subject = 'Subject';
-$mail->Body = 'Message body';
+$mail->Subject = 'Новая заявка';
+$mail->Body = 'Тут будет таблица с данными';
 
 if ($mail->send()) {
     echo '✅ Письмо отправлено ))))';
