c3fbddbd0023bb0e91f14d9aa013c3248ebf1391
Max <gorbatenkomax@yandex.ru>
2024-04-12 23:32:25 +0300

    update

--- pages/orders/orders.js
+++ pages/orders/orders.js
@@ -1,29 +1,32 @@
 export default function orders() {
     const accessToken = '7edeef28895a8bacd3b5ae2e21578ed23eb2a58f';
 
+    let username = "admin@luxuryworldluxe";
+    let password = "0ef4b349bb";
+
     // const getTovar = XMLHttpRequestAJAX({
-    //     url: "https://online.moysklad.ru/api/remap/1.2/entity/product",
+    //     url: "https://api.moysklad.ru/api/remap/1.2/entity/product",
     //     method: "GET",
     //     headers: {
-    //         'Authorization': 'Bearer ' + accessToken,
+    //         'Authorization': 'Bearer 7edeef28895a8bacd3b5ae2e21578ed23eb2a58f',
     //         'Content-Type': 'application/json'
     //     }
     // });
     //
     // console.log("getTovar", getTovar)
 
-    fetch('https://online.moysklad.ru/api/remap/1.2/entity/product', {
-        method: 'GET',
-        headers: {
-            'Authorization': 'Bearer ' + accessToken,
-            'Content-Type': 'application/json'
-        }
+
+
+
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer 7edeef28895a8bacd3b5ae2e21578ed23eb2a58f");
+
+    fetch("https://api.moysklad.ru/api/remap/1.2/entity/product", {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow"
     })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+        .then((response) => response.text())
+        .then((result) => console.log(result))
+        .catch((error) => console.error(error));
 }
