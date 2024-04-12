c8f686ec4f6283eafe4a07648207e7dac6288408
Max <gorbatenkomax@yandex.ru>
2024-04-13 00:02:18 +0300

    update

--- pages/orders/orders.js
+++ pages/orders/orders.js
@@ -1,31 +1,34 @@
 export default function orders() {
-    const accessToken = '7edeef28895a8bacd3b5ae2e21578ed23eb2a58f';
 
+    const token = '7edeef28895a8bacd3b5ae2e21578ed23eb2a58f';
     let username = "admin@luxuryworldluxe";
     let password = "0ef4b349bb";
+    let login = btoa(username + ":" + password);
+
 
     // const getTovar = XMLHttpRequestAJAX({
     //     url: "https://api.moysklad.ru/api/remap/1.2/entity/product",
     //     method: "GET",
     //     headers: {
-    //         'Authorization': 'Bearer 7edeef28895a8bacd3b5ae2e21578ed23eb2a58f',
-    //         'Content-Type': 'application/json'
+    //         'Authorization': 'Basic ' + login,
+    //         'Accept-Encoding:': 'gzip',
+    //         'Lognex-Pretty-Print-JSON': 'true',
     //     }
     // });
     //
     // console.log("getTovar", getTovar)
 
 
-
-
     const myHeaders = new Headers();
     myHeaders.append("Authorization", "Bearer 7edeef28895a8bacd3b5ae2e21578ed23eb2a58f");
 
-    fetch("https://api.moysklad.ru/api/remap/1.2/entity/product", {
+    const requestOptions = {
         method: "GET",
         headers: myHeaders,
-        redirect: "follow"
-    })
+        redirect: "manual"
+    };
+
+    fetch("https://api.moysklad.ru/api/remap/1.2/entity/product", requestOptions)
         .then((response) => response.text())
         .then((result) => console.log(result))
         .catch((error) => console.error(error));
