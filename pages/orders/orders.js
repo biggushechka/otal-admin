edb2dffb844c35b1dc9e5511ceb62444143d506b
Max <gorbatenkomax@yandex.ru>
2024-04-13 09:45:08 +0300

    update

--- pages/orders/orders.js
+++ pages/orders/orders.js
@@ -1,35 +1,29 @@
 export default function orders() {
 
+    var btn = document.createElement("div");
+    btn.innerHTML = `<button type="button" class="btn btn-primary">Получить</button>`;
+    document.querySelector("#app").append(btn);
+
     const token = '7edeef28895a8bacd3b5ae2e21578ed23eb2a58f';
+    const urlProducts = "https://api.moysklad.ru/api/remap/1.2/entity/product";
     let username = "admin@luxuryworldluxe";
     let password = "0ef4b349bb";
     let login = btoa(username + ":" + password);
 
 
-    // const getTovar = XMLHttpRequestAJAX({
-    //     url: "https://api.moysklad.ru/api/remap/1.2/entity/product",
-    //     method: "GET",
-    //     headers: {
-    //         'Authorization': 'Basic ' + login,
-    //         'Accept-Encoding:': 'gzip',
-    //         'Lognex-Pretty-Print-JSON': 'true',
-    //     }
-    // });
-    //
-    // console.log("getTovar", getTovar)
+    btn.addEventListener("click", function () {
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", "Bearer "+token);
 
+        const requestOptions = {
+            method: "GET",
+            headers: myHeaders,
+            redirect: "manual"
+        };
 
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer 7edeef28895a8bacd3b5ae2e21578ed23eb2a58f");
-
-    const requestOptions = {
-        method: "GET",
-        headers: myHeaders,
-        redirect: "manual"
-    };
-
-    fetch("https://api.moysklad.ru/api/remap/1.2/entity/product", requestOptions)
-        .then((response) => response.text())
-        .then((result) => console.log(result))
-        .catch((error) => console.error(error));
+        fetch("https://api.moysklad.ru/api/remap/1.2/entity/product", requestOptions)
+            .then((response) => response.text())
+            .then((result) => console.log(result))
+            .catch((error) => console.error(error));
+    });
 }
