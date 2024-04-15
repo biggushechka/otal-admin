4a598c164a3a835e71be2b7fb844f8c11ca21d2c
Max <gorbatenkomax@yandex.ru>
2024-04-15 23:19:58 +0300

    update

--- pages/orders/orders.js
+++ pages/orders/orders.js
@@ -4,26 +4,18 @@ export default function orders() {
     btn.innerHTML = `<button type="button" class="btn btn-primary">Получить</button>`;
     document.querySelector("#app").append(btn);
 
-    const token = '7edeef28895a8bacd3b5ae2e21578ed23eb2a58f';
-    const urlProducts = "https://api.moysklad.ru/api/remap/1.2/entity/product";
-    let username = "admin@luxuryworldluxe";
-    let password = "0ef4b349bb";
-    let login = btoa(username + ":" + password);
-
+    const token = 'uNb8f87WSiQht53OeUO3HK6WFjppmMPL';
+    const urlGetProducts = 'https://musorhren.retailcrm.ru/api/v5/store/products';
 
     btn.addEventListener("click", function () {
-        const myHeaders = new Headers();
-        myHeaders.append("Authorization", "Bearer "+token);
-
-        const requestOptions = {
+        var maxmax = XMLHttpRequestAJAX({
+            url: urlGetProducts,
             method: "GET",
-            headers: myHeaders,
-            redirect: "manual"
-        };
+            headers: {
+                "X-Api-Key": token
+            }
+        });
 
-        fetch("https://api.moysklad.ru/api/remap/1.2/entity/product", requestOptions)
-            .then((response) => response.text())
-            .then((result) => console.log(result))
-            .catch((error) => console.error(error));
+        console.log("maxmax", maxmax)
     });
 }
