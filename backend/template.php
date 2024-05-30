877be718ddbdc8f451131789560492a2b4d75a93
gorbatenkomax.formula@yandex.ru <gorbatenkomax.formula@yandex.ru>
2024-05-30 12:28:14 +0300

    update

--- backend/template.php
+++ backend/template.php
@@ -5,18 +5,19 @@ $version_new = "";
 $root = $_SERVER['DOCUMENT_ROOT'];
 require_once $root . '/backend/clearCash.php';
 
-if ($_SERVER['HTTP_HOST'] != 'otal-estate.ru') {
+if ($_SERVER['HTTP_HOST'] !== "otal-estate.ru") {
     $version = mt_rand(10000, 99999999);
 } else {
-    $getFileVersion = file($root."/backend/version.txt", FILE_IGNORE_NEW_LINES);
+    $getFileVersion = file($root . "/backend/version.txt", FILE_IGNORE_NEW_LINES);
     $version_now = $getFileVersion[0];
     $version_new = $getFileVersion[1];
+    $version = $version_now;
 
     if ($version_now != $version_new) {
-        $version = $version_new;
         $version_now = $version_new;
+        $version = $version_now;
         $getFileVersion[0] = $getFileVersion[1];
-        file_put_contents($root."/backend/version.txt", implode(PHP_EOL, $getFileVersion));
+        file_put_contents($root . "/backend/version.txt", implode(PHP_EOL, $getFileVersion));
 
         clearCash($root . "/assets", $version);
         clearCash($root . "/css", $version);
