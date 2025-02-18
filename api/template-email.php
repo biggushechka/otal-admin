812fd0ac13d8d549d0743bb4d1331f264a6f422b
Max <gorbatenkomax@yandex.ru>
2025-02-19 00:13:16 +0300

    update

--- api/template-email.php
+++ api/template-email.php
@@ -1,5 +1,10 @@
 <?php
 
+// разрешаем подключаться к API
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
+
 $rootPath = $_SERVER['DOCUMENT_ROOT'];
 $method = $_SERVER['REQUEST_METHOD'];
 
